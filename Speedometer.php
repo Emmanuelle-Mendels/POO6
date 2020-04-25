49736a2a2b83d333c075cc65ed16cece361392b0
Melusine45 <emmanuelle.mendels@gmail.com>
2020-04-25 18:25:05 +0200

    ajout d'une constante

--- Speedometer.php
+++ Speedometer.php
@@ -3,14 +3,15 @@
 
 class Speedometer
 {
+    const distance = 0.621;
 
     public static function convertKmtoMiles( int $km) : float
     {
-        return $km  * 0.621;
+        return round($km  * self::distance,2);
     }
 
     public static function convertMilestoKm(int $miles) : float
     {
-        return $miles * 1.61;
+        return round($miles / self::distance,2);
     }
 }
